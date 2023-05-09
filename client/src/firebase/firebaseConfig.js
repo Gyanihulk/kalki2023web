e3927eb7881385bbed27d1c2d92491ec7c177241
Adamya028 <adamya@appfoster.com>
2023-05-09 14:12:28 +0530

    Feat/ document upload

--- client/src/firebase/firebaseConfig.js
+++ client/src/firebase/firebaseConfig.js
@@ -1,7 +1,10 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,5 +24,6 @@ export const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
-const db = getFirestore();
-export { app, auth, db };
+const db = getFirestore(app);
+const storage = getStorage(app);
+export { app, auth, db ,storage};
